2c32a095435fdd4f1813fe661ceee0e4e2873064
Isaac Yong <isaacyong0804@gmail.com>
2022-03-21 20:12:37 +0800

    Utils and HiPIO class

--- model.py
+++ model.py
@@ -5,26 +5,27 @@ import math
 import matplotlib.pyplot as plt
 from utils import imshow
 
-# Implementation of HiP-16
+# Implementation of HiP-16 and HiP-256
 
 
 class SelfAttentionLayer(nn.Module):
-    def __init__(self, embed_dim, num_heads):
+    def __init__(self, embed_dim, num_heads, expanding_factor=4):
         super().__init__()
         self.mha = nn.MultiheadAttention(embed_dim, num_heads)
-        self.linear = nn.Linear(embed_dim, embed_dim)
+        self.linear1 = nn.Linear(embed_dim, embed_dim * expanding_factor)
+        self.linear2 = nn.Linear(embed_dim * expanding_factor, embed_dim)
         self.norm1 = nn.LayerNorm(embed_dim)
         self.norm2 = nn.LayerNorm(embed_dim)
 
     def forward(self, x):
         x = self.norm1(self.mha(x, x, x)[0] + x)
-        x = self.norm2(self.linear(x) + x)
+        x = self.norm2(F.gelu(self.linear2(F.gelu(self.linear1(x)))) + x)
         return x
 
 
 class HiPLayer(nn.Module):
     def __init__(self, num_groups, num_attn_layers, num_latents, num_channels,
-                 num_heads, input_dim=None):
+                 num_heads, input_channels=None):
         super().__init__()
         self.num_groups = num_groups
         self.num_attn_layers = num_attn_layers
@@ -33,15 +34,15 @@ class HiPLayer(nn.Module):
         self.num_heads = num_heads
         self.dot_scale = 1 / math.sqrt(num_channels)
 
-        input_dim = num_channels if input_dim == None else input_dim
-        self.linear = nn.Linear(input_dim, num_channels)
+        input_channels = num_channels if input_channels == None else input_channels
+        self.linear = nn.Linear(input_channels, num_channels)
         self.learned_embedding = nn.Parameter(
             torch.randn(num_groups, num_latents, num_channels))
-        self.attention = nn.MultiheadAttention(input_dim, num_heads)
+        self.attention = nn.MultiheadAttention(input_channels, num_heads)
 
         self.stem = nn.ModuleList(
             [SelfAttentionLayer(num_channels, num_heads)
-            for _ in range(num_attn_layers)]
+             for _ in range(num_attn_layers)]
         )
 
     def forward(self, x):
@@ -71,37 +72,75 @@ class HiPLayer(nn.Module):
         return x, attn
 
 
+class HiP16_small(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.ModuleList(
+            [
+                HiPLayer(num_groups=16, num_attn_layers=2, num_latents=32,
+                         num_channels=64, num_heads=4, input_channels=32),
+                HiPLayer(num_groups=4, num_attn_layers=2, num_latents=64,
+                         num_channels=128, num_heads=8, input_channels=64),
+                HiPLayer(num_groups=1, num_attn_layers=18, num_latents=128,
+                         num_channels=256, num_heads=16, input_channels=128),
+                HiPLayer(num_groups=1, num_attn_layers=2, num_latents=64,
+                         num_channels=512, num_heads=32, input_channels=256),
+            ]
+        )
+        self.decoder = nn.ModuleList(
+            [
+                HiPLayer(num_groups=1, num_attn_layers=1, num_latents=128,
+                         num_channels=256, num_heads=16, input_channels=512),
+                HiPLayer(num_groups=4, num_attn_layers=1, num_latents=64,
+                         num_channels=128, num_heads=8, input_channels=256),
+                HiPLayer(num_groups=16, num_attn_layers=1, num_latents=32,
+                         num_channels=64, num_heads=4, input_channels=128)
+            ]
+        )
+
+    def forward(self, x):
+        skips = []
+        for _, layer in enumerate(self.encoder):
+            skips.append(x)
+            x = layer(x)[0]
+        for _, layer in enumerate(self.decoder):
+            x = layer(x)[0] + skips.pop()
+        return x
+
+
 class HiP16(nn.Module):
     def __init__(self):
         super().__init__()
         self.encoder = nn.ModuleList(
             [
                 HiPLayer(num_groups=16, num_attn_layers=2, num_latents=128,
-                     num_channels=128, num_heads=4, input_dim=32),
+                         num_channels=128, num_heads=4, input_channels=32),
                 HiPLayer(num_groups=4, num_attn_layers=2, num_latents=256,
-                     num_channels=256, num_heads=8, input_dim=128),
+                         num_channels=256, num_heads=8, input_channels=128),
                 HiPLayer(num_groups=1, num_attn_layers=18, num_latents=256,
-                     num_channels=512, num_heads=16, input_dim=256),
+                         num_channels=512, num_heads=16, input_channels=256),
                 HiPLayer(num_groups=1, num_attn_layers=2, num_latents=64,
-                     num_channels=1024, num_heads=32, input_dim=512),
+                         num_channels=1024, num_heads=32, input_channels=512),
             ]
         )
         self.decoder = nn.ModuleList(
             [
                 HiPLayer(num_groups=1, num_attn_layers=1, num_latents=256,
-                     num_channels=512, num_heads=16, input_dim=1024),
+                         num_channels=512, num_heads=16, input_channels=1024),
                 HiPLayer(num_groups=4, num_attn_layers=1, num_latents=256,
-                     num_channels=256, num_heads=8, input_dim=512),
+                         num_channels=256, num_heads=8, input_channels=512),
                 HiPLayer(num_groups=16, num_attn_layers=1, num_latents=128,
-                     num_channels=128, num_heads=4, input_dim=256)
+                         num_channels=128, num_heads=4, input_channels=256)
             ]
         )
 
     def forward(self, x):
+        skips = []
         for _, layer in enumerate(self.encoder):
+            skips.append(x)
             x = layer(x)[0]
         for _, layer in enumerate(self.decoder):
-            x = layer(x)[0]
+            x = layer(x)[0] + skips.pop()
         return x
 
 
@@ -111,35 +150,37 @@ class HiP256(nn.Module):
         self.encoder = nn.ModuleList(
             [
                 HiPLayer(num_groups=256, num_attn_layers=1, num_latents=32,
-                     num_channels=64, num_heads=1, input_dim=16),
+                         num_channels=64, num_heads=1, input_channels=16),
                 HiPLayer(num_groups=64, num_attn_layers=1, num_latents=64,
-                     num_channels=96, num_heads=2, input_dim=64),
+                         num_channels=96, num_heads=2, input_channels=64),
                 HiPLayer(num_groups=16, num_attn_layers=2, num_latents=128,
-                     num_channels=128, num_heads=4, input_dim=96),
+                         num_channels=128, num_heads=4, input_channels=96),
                 HiPLayer(num_groups=4, num_attn_layers=2, num_latents=256,
-                     num_channels=256, num_heads=8, input_dim=128),
+                         num_channels=256, num_heads=8, input_channels=128),
                 HiPLayer(num_groups=1, num_attn_layers=18, num_latents=256,
-                     num_channels=512, num_heads=16, input_dim=256),
+                         num_channels=512, num_heads=16, input_channels=256),
                 HiPLayer(num_groups=1, num_attn_layers=2, num_latents=64,
-                     num_channels=1024, num_heads=32, input_dim=512),
+                         num_channels=1024, num_heads=32, input_channels=512),
             ]
         )
         self.decoder = nn.ModuleList(
             [
                 HiPLayer(num_groups=1, num_attn_layers=1, num_latents=256,
-                     num_channels=256, num_heads=16, input_dim=1024),
+                         num_channels=256, num_heads=16, input_channels=1024),
                 HiPLayer(num_groups=4, num_attn_layers=1, num_latents=256,
-                     num_channels=128, num_heads=8, input_dim=256),
+                         num_channels=128, num_heads=8, input_channels=256),
                 HiPLayer(num_groups=16, num_attn_layers=1, num_latents=128,
-                     num_channels=64, num_heads=4, input_dim=128),
+                         num_channels=64, num_heads=4, input_channels=128),
                 HiPLayer(num_groups=64, num_attn_layers=1, num_latents=64,
-                     num_channels=32, num_heads=2, input_dim=64),
+                         num_channels=32, num_heads=2, input_channels=64),
                 HiPLayer(num_groups=256, num_attn_layers=1, num_latents=32,
-                     num_channels=16, num_heads=1, input_dim=32)
+                         num_channels=16, num_heads=1, input_channels=32)
             ]
         )
 
     def forward(self, x):
+        # Skip connection is not yet implemented for HiP-256 because the shape
+        # doesn't match according to paper setup.
         for _, layer in enumerate(self.encoder):
             x = layer(x)[0]
         for _, layer in enumerate(self.decoder):
@@ -147,10 +188,31 @@ class HiP256(nn.Module):
         return x
 
 
+class HiPIO(nn.Module):
+    def __init__(self, preprocess_layer, backbone, postprocess_layer):
+        super().__init__()
+        self.preprocess_layer = preprocess_layer
+        self.backbone = backbone
+        self.postprocess_layer = postprocess_layer
+
+    def forward(self, x):
+        x = self.preprocess_layer(x)
+        x = self.backbone(x)
+        x = self.postprocess_layer(x)
+        return x
+
+
+
 if __name__ == "__main__":
 
+    hip16s = HiP16_small()
     hip16 = HiP16()
     hip256 = HiP256()
+
+    x = torch.randn(1, 1024, 32)
+    y = hip16s(x)
+    print(y.shape)
+
     x = torch.randn(1, 1024, 32)
     y = hip16(x)
     print(y.shape)
@@ -158,3 +220,11 @@ if __name__ == "__main__":
     x = torch.randn(1, 1024, 16)
     y = hip256(x)
     print(y.shape)
+
+    model = HiPIO(preprocess_layer, hip16s, postprocess_layer)
+
+    # x = torch.randn(2, 3, 32, 32)
+    x = torch.randn(2, 3, 16, 16)
+    y = model(x)
+    print(y.shape)
+    imshow(y[0], batched=False)
--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ def is_prime(n: int):
     return True, n
 
 
-def imshow(inputs, nrow=None, ncol=None):
+def imshow(inputs, nrow=None, ncol=None, batched=True, channel_first=False):
     """
     Show the images.
 
@@ -26,13 +26,16 @@ def imshow(inputs, nrow=None, ncol=None):
     if isinstance(inputs, torch.Tensor):
         inputs = inputs.detach().numpy()
 
-    if (inputs.ndim > 3 or inputs.ndim < 2):
-        raise ValueError("Input must be 3 dimensional or 2 dimensional but, "
-                        f"receive input of shape{inputs.shape}""")
-    elif inputs.ndim == 2:
-        plt.imshow(inputs)
-        plt.show()
-    elif inputs.ndim == 3:
+    if batched:
+        if (inputs.ndim > 4 or inputs.ndim < 3):
+            raise ValueError("Input must be 4 dimensional or 3 dimensional but, "
+                            f"receive input of shape{inputs.shape}""")
+
+        # Permute if channel_first
+        if inputs.ndim == 4 and channel_first:
+            inputs = np.transpose(inputs, (0, 2, 3, 1))
+
+        # Finding Grid rows and columns
         if nrow:
             if ncol:
                 assert (inputs.shape[0] == nrow * ncol), \
@@ -41,12 +44,12 @@ def imshow(inputs, nrow=None, ncol=None):
                 "Value of nrow must divide inputs.shape[0]"
         else:
             if is_prime(inputs.shape[0])[0]:
-                # assert (inputs.shape[0] < 7), \
-                # "inputs.shape[0] is a prime number, please specify nrow (and ncol)"
                 print("inputs.shape[0] is a prime number")
                 nrow = inputs.shape[0]
             else: 
                 nrow = is_prime(inputs.shape[0])[1]
+
+        # Actual Plotting
         ncol = inputs.shape[0] // nrow
         fig, ax = plt.subplots(nrow, ncol)
         print(f"Showing {inputs.shape[0]} 2d map in {nrow} x {ncol} grid.")
@@ -57,8 +60,16 @@ def imshow(inputs, nrow=None, ncol=None):
             for ii in range(inputs.shape[0]):
                 ax[ii].imshow(inputs[ii])
         plt.show()
+    else: 
+        # Permute if channel_first
+        if inputs.ndim == 3 and channel_first:
+            inputs = np.transpose(inputs, (1, 2, 0))
+
+        # Actual Plotting
+        plt.imshow(inputs)
+        plt.show()
 
 
 if __name__ == "__main__":
     a = torch.randn(12, 32, 32)
-    imshow(a, nrow=3)
+    imshow(a, nrow=3, channel_first=True)
